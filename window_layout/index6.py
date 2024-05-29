9e3b00332ba7e06d5dd430ce7a4edeab1c1114a9
Ian092 <bhbine@gmail.com>
2024-05-29 10:30:39 +0800

    modify

--- window_layout/index6.py
+++ window_layout/index6.py
@@ -7,11 +7,11 @@ class Window(tk.Tk):
           self.title("Ian")
           self.geometry("300x200")
 
-          ttk.Button(self,text="Top").pack(fill="x")
+          ttk.Button(self,text="Top").pack(fill="x",padx=10)
           
-          ttk.Button(self,text="Middle").pack(fill="x")
+          ttk.Button(self,text="Middle").pack(fill="x",padx=20)
 
-          ttk.Button(self,text="Bottom").pack(fill="x")
+          ttk.Button(self,text="Bottom").pack(fill="x",padx=30)
 
 
 if __name__=="__main__":
