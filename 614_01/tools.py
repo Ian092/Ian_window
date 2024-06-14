b1c0fa7f7d321fa74afc11ba6be630fa363a8736
Ian <bhbine@gmail.com>
2024-06-14 16:06:00 +0800

    modify

---
+++ 614_01/tools.py
@@ -0,0 +1,44 @@
+from tkinter.simpledialog import Dialog
+from tkinter import ttk
+from tkinter import Misc
+import tkinter as tk
+from data import Info
+import tkintermapview as tkmap # type: ignore
+
+class CustomMessagebox(Dialog):    
+    def __init__(self, parent:Misc, title:str,site:Info):        
+        print(site)
+        super().__init__(parent=parent, title=title)
+
+    def body(self, master):
+        # 創建對話框主體。返回應具有初始焦點的控件。
+        contain_frame = ttk.Frame(master)
+        #====================    
+        map_frame = ttk.Frame(contain_frame)
+        map_widget = tkmap.TkinterMapView(map_frame,
+                                         width=800,
+                                         height=600,
+                                         corner_radius=0
+                                         )
+        map_widget.pack()
+        map_frame.pack(expand=True,fill='both')
+        #===================
+        contain_frame.pack(expand=True,fill='both',pady=10,padx=30)
+
+    def apply(self):
+        # 當用戶按下確定時處理數據
+        pass
+
+    def buttonbox(self):
+        # Add custom buttons (overriding the default buttonbox)
+        box = ttk.Frame(self)
+        self.ok_button = tk.Button(box, text="確定", width=10, command=self.ok, default=tk.ACTIVE)
+        self.ok_button.pack(side=tk.LEFT, padx=5, pady=5)
+        box.pack()
+
+    def ok(self):
+        # Override the ok method
+        super().ok()
+
+
+    
