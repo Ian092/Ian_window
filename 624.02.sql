50f90b38353a0bd95df19d9e8b6a91d6d46f36b7
Ian092 <bhbine@gmail.com>
2024-06-24 11:52:25 +0800

    modify

---
+++ 624.02.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS student (
+	student_id serial Primary key,
+    name Varchar(20) not null,
+    major Varchar(20)
+);
---
+++ 624SQL.sql
@@ -0,0 +1,20 @@
+CREATE TABLE student(
+	student_id SERIAL PRIMARY KEY,
+	name VARCHAR(20),
+	major VARCHAR(20)
+);
+
+CREATE TABLE accounts (
+  user_id SERIAL PRIMARY KEY, 
+  username VARCHAR (50) UNIQUE NOT NULL, 
+  password VARCHAR (50) NOT NULL, 
+  email VARCHAR (255) UNIQUE NOT NULL, 
+  created_at TIMESTAMP NOT NULL, 
+  last_login TIMESTAMP
+);
+
+INSERT INTO student(name,major)
+VALUES ('Ian','學生');
+
+DROP TABLE accounts;
+DROP TABLE student;
