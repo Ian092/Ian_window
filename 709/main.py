d15fc57962e682ad8dd10c7102f044c83be41253
Ian092 <bhbine@gmail.com>
2024-07-09 11:55:53 +0800

    modify

--- 709/main.py
+++ 709/main.py
@@ -3,5 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return "<h1>我的主題</h1>\n<h2>職能發展學院</h2>"
+
+@app.route('/hello')
+def hello():
+    return '<h1>Hello, World</h1>'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'<h1>Hello! {username}</h1>'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'<h1>Post {post_id}</h1>'
+
---
+++ 709/main1.py
@@ -0,0 +1,25 @@
+from flask import Flask,url_for
+
+
+app=Flask(__name__)
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login abc'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='Ian',password="1234"))
+    print(url_for('profile', username='John Doe'))
+    
+    
+    
+    
